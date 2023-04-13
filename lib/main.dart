0384832730bfe75c110d4490f326ad7953fc3652
Aditi Ashish Gawande <68374073+AditiGawande4@users.noreply.github.com>
2023-04-13 18:08:00 +0530

    Completed Project

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    var player = AudioPlayer();
+    player.play(AssetSource('note$soundNumber.wav'));
+  }
+
+  Expanded buildKey({int soundNumber, Color color}) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        style: TextButton.styleFrom(
+          backgroundColor: color,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                buildKey(soundNumber: 1, color: Colors.red),
+                buildKey(soundNumber: 2, color: Colors.orange),
+                buildKey(soundNumber: 3, color: Colors.yellow),
+                buildKey(soundNumber: 4, color: Colors.green),
+                buildKey(soundNumber: 5, color: Colors.teal),
+                buildKey(soundNumber: 6, color: Colors.blue),
+                buildKey(soundNumber: 7, color: Colors.purple)
+              ]),
+        ),
+      ),
+    );
+  }
+}
